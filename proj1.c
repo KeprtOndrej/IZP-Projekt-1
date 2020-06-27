5687be478ed6700911a9a71d23d0d8fd8d6c58a1
Ondrej Keprt <keprt.ondrej@gmail.com>
2020-06-27 18:59:05 +0200

    Version 2 + seznam1 added

--- proj1.c
+++ proj1.c
@@ -3,36 +3,45 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
-	int radek, znak;
-	char string[1000][100];
-	radek = znak = 0;
-	
-	//nacteni ze seznamu
-	while ((c=fgetc(stdin)) != EOF)
+	//overeni poctu hledanych cisel
+	if (argc > 2)
 	{
-		while (c != '\n')
-			{
-			/*	while (c != '\0')				//tohle cast kodu kdyz se odkomentaruje tak mi to vyhodi vetu, ze saham nekam, kde nemam pristum
-				{						// kdyz to je takhle zakomentarovane, tak se mi dodrbe prikazova radka(jakoch porad psal argumenty si myslim..)
-					string[radek][znak] = c;
-					znak++;
-				}	*/
-			znak = 0;
-			}
-		radek++;
+		printf("zadej poze jedno hledane cislo\n");
+		return 1;
 	}
-	
+
+	//nacteni hodnot
+	char c[100];			// 100+-2
+	char jmeno[100][100];		 //100+-2
+	char cislo[100][100];	 	// 10+-2
+	int radek = 0;
+
+	while (fgets(c,100,stdin) !=NULL)
+		{
+			strcpy(jmeno[radek][0],c);
+			radek++;
+		}
+
+	//vypsani
+	radek = 0;
+	while (jmeno[radek][0] != NULL)
+		{
+			puts(jmeno[radek][0]);
+			radek++;
+		}
+
+
+
+
 
 
 	//porovnani
 	//one day maybe xD
 
 	//kvuli prekladaci protoze je zatim nepouzivam
-	string[2][3] = 'a';
-	printf("%s",argv[1]);
-	printf("%d",argc);
-	printf("%c",string[2][3]);
+//	string[2][3] = 'a';
+//	printf("%s",argv[1]);
+//	printf("%d",argc);
+//	printf("%c",string[2][3]);
 	return 0;
 }
-
