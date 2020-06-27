7f1bd21cf70a613ad497705a434816f3e81a0537
Ondrej Keprt <keprt.ondrej@gmail.com>
2020-06-27 19:01:12 +0200

    version 3

--- proj1.c
+++ proj1.c
@@ -1,47 +1,190 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(int argc, char *argv[])
+#define pocet_kontaktu 1000
+#define pocet_znaku 100
+
+void prevod(int i,char original[pocet_kontaktu][pocet_znaku],char kopie[pocet_kontaktu][pocet_znaku]);
+
+int main (int argc, char *argv[])
 {
-	//overeni poctu hledanych cisel
+	//overeni poctu argumentu
 	if (argc > 2)
 	{
-		printf("zadej poze jedno hledane cislo\n");
+		fprintf(stderr,"zadej pouze jedno hledane cislo\n");
 		return 1;
 	}
 
-	//nacteni hodnot
-	char c[100];			// 100+-2
-	char jmeno[100][100];		 //100+-2
-	char cislo[100][100];	 	// 10+-2
-	int radek = 0;
+	//ulozeni seznamu
+	char c;	//pomocna promena pro nacitani
+	int kontakt = 0;
+	int znak = 0;
+	int pocet_zaznamu = 0;
+	char jmeno[pocet_kontaktu][pocet_znaku];
+	char cislo[pocet_kontaktu][pocet_znaku];
+	while ((c=fgetc(stdin)) != EOF)
+	{
+		if ((pocet_zaznamu % 2) == 1)
+			{
+				if (c != '\n')
+				{
+					cislo[kontakt][znak] = c;
+					znak++;
+				}
+				else
+				{
+					pocet_zaznamu++;
+					kontakt++;
+					znak = 0;
+				}
+			}
+			else
+			{
+				if (c != '\n')
+				{
+					jmeno[kontakt][znak] = tolower(c);
+					znak++;
+				}
+				else
+				{
+					//printf("%s\n", zaznam[radek]);
+					pocet_zaznamu++;
+					znak = 0;
+				}
+			}
+	}
+pocet_zaznamu = (pocet_zaznamu/2)+1;
 
-	while (fgets(c,100,stdin) !=NULL)
+//vypis bez zadani hledaneho cisla
+if (argc == 1)
+	{
+
+			for (kontakt = 0; kontakt < pocet_zaznamu-1;kontakt++)
+				{
+					printf("%s, %s\n",jmeno[kontakt],cislo[kontakt]);
+				}
+		return 0;
+	}
+
+
+// prevod jmena na cisla
+char kontakt_cisla [pocet_kontaktu][pocet_znaku];
+char cislo_cisla [pocet_kontaktu][pocet_znaku];
+for (int i = 0; i < pocet_zaznamu;i++)
+{
+prevod(i,jmeno,kontakt_cisla);
+prevod(i,cislo,cislo_cisla);
+znak = 0;
+}
+
+
+// pole kterre rika, jake zaznami jsme nalezly
+int vypsat[pocet_zaznamu];
+ for (kontakt = 0; kontakt < pocet_zaznamu;kontakt++)
+ {
+	 vypsat[kontakt] = 0;
+ }
+
+ //porovnani
+ char *p;
+ int nalezeno = 0;
+for (kontakt = 0; kontakt < pocet_zaznamu;kontakt++)
 		{
-			strcpy(jmeno[radek][0],c);
-			radek++;
-		}
+			//porovnani cisel
+			p = strstr(cislo_cisla[kontakt], argv[1]);
+			if (p != NULL)
+				{
+					vypsat[kontakt] = 1;
+					nalezeno = 1;
+				}
 
-	//vypsani
-	radek = 0;
-	while (jmeno[radek][0] != NULL)
+				// porovnani jmen
+				p = strstr(kontakt_cisla[kontakt], argv[1]);
+				if (p != NULL)
+					{
+						vypsat[kontakt] = 1;
+						nalezeno = 1;
+					}
+		}
+//vypis nalezenych kontaktu
+	for (int kontakt = 0; kontakt <pocet_zaznamu;kontakt++)
+	{
+		if (vypsat[kontakt])
 		{
-			puts(jmeno[radek][0]);
-			radek++;
+			printf("%s, %s  // %s // %s\n",jmeno[kontakt],cislo[kontakt],kontakt_cisla[kontakt],cislo_cisla[kontakt]);
 		}
+	}
 
-
-
-
-
-
-	//porovnani
-	//one day maybe xD
-
-	//kvuli prekladaci protoze je zatim nepouzivam
-//	string[2][3] = 'a';
-//	printf("%s",argv[1]);
-//	printf("%d",argc);
-//	printf("%c",string[2][3]);
+	//nebyla nalezena zadna shoda
+	if (nalezeno == 0)
+	{
+		printf("Not found\n");
+	}
 	return 0;
+}
+
+void prevod (int i, char original[pocet_kontaktu][pocet_znaku],char kopie[pocet_kontaktu][pocet_znaku])
+{
+	int znak = 0;
+		while (original[i][znak] !='\0')
+		{
+			switch (original[i][znak])
+			{
+				case ' ':
+				kopie[i][znak] = ' ';
+				break;			// znak mezery
+				case 'a':
+				case 'b':
+				case 'c':
+					kopie[i][znak] = '2';
+					break;
+				case 'd':
+				case 'f':
+				case 'e':
+					kopie[i][znak] = '3';
+				break;
+				case 'g':
+				case 'h':
+				case 'i':
+					kopie[i][znak] = '4';
+					break;
+				case 'j':
+				case 'k':
+				case 'l':
+					kopie[i][znak] = '5';
+				break;
+				case 'm':
+				case 'n':
+				case 'o':
+					kopie[i][znak] = '6';
+					break;
+				case 'p':
+				case 'q':
+				case 'r':
+				case 's':
+					kopie[i][znak] = '7';
+				break;
+				case 't':
+				case 'u':
+				case 'v':
+					kopie[i][znak] = '8';
+					break;
+				case 'w':
+				case 'x':
+				case 'y':
+				case 'z':
+					kopie[i][znak] = '9';
+				 break;
+				case '+':
+				kopie[i][znak] = '0';
+				 break;
+				default:
+					kopie[i][znak] = original[i][znak];
+					break;
+			 }
+			znak++;
+		}
+
+
 }
